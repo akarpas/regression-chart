9c34e2e2df764a076fc6b7395fa1ef6c3f1ade9a
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-21 13:05:40 +0200

    feat: fetch data to component from api

--- client/src/components/LinearRegressionChart.js
+++ client/src/components/LinearRegressionChart.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LinearRegressionChart = () => {
+  const [graphData, setGraphData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/v1/data')
+      .then(response => response.json())
+      .then(data => {
+        setGraphData(data);
+        drawChart();
+      })
+    return;
+  }, []);
+
+  const drawChart = () => {}
+
   return (
     <div>
       Graph
