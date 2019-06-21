cb86d7a68f7056643ea94e1d4d605ecbf0f4085f
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-21 11:30:19 +0200

    feat: setup server on port 3000

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const DEFAULT_PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || DEFAULT_PORT;
+
+const router = express.Router();
+
+app.use('/api/v1', router);
+
+router.get('/data', (req, res) => {
+  res.send({
+    status: 'ok',
+    statusCode: 200,
+    data: {}
+  });
+});
+
+app.listen(port);
+console.log('Server is listening on port', port);
