a073114b6f903b25e92baf7761a3a6b4aa1f7cda
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-21 12:02:17 +0200

    docs: explain data calculation

--- server/utils/data.js
+++ server/utils/data.js
@@ -3,11 +3,15 @@ const calcData = (dataSets) => {
   let data = {};
 
   for (let i = 0; i < totalDataSets; i++) {
+
+    // Set dynamic server name
     const serverName = `server_${i + 1}`;
     data[serverName] = [];
     for (let j = 0; j < 15; j++) {
       const set = {
         serverLoad: (Math.random() * 100),
+        // Add i to make each dataset value a bit larger than previous
+        // for presentation reasons
         responseTime: Math.random() + i,
         processingPower: Math.random() + i,
       };
