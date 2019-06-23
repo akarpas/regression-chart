f33f027dcb2ecb85712e6c3f9131643a1c578838
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-23 11:49:24 +0200

    chore: remove regression calculation from parseData

--- client/src/utils/dataParser.js
+++ client/src/utils/dataParser.js
@@ -2,19 +2,12 @@ import regression from 'regression';
 
 const parseData = (data, columnType) => {
   return Object.keys(data).map((key, index) => {
-    const xyData = data[key].map(item => {
-      return [item.serverLoad, item[columnType]];
-    })
-    const regressionData = regression.linear(xyData);
-    const { points } = regressionData;
-
     return {
       server: key,
       points: data[key].map((item, index) => {
         return {
           x: item.serverLoad,
           y: item[columnType],
-          yhat: points[index][1]
         };
       })
     }
