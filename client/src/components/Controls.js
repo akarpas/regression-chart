48c67beb531ed3604df7eea5aa39a89b7c60578a
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-23 14:05:51 +0200

    feat: create basic controls component

---
+++ client/src/components/Controls.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useLayoutEffect, useState } from "react";
+
+import style from './Controls.module.scss';
+
+const Controls = props => {
+
+    return (
+        <div className="chart-container">
+        </div>
+    );
+};
+
+export default Controls;
