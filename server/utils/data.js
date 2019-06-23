2c37d7a8b5160a6f7c89e6d3ce48f8422797ae4f
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-23 13:27:22 +0200

    chore: set random decling chart

--- server/utils/data.js
+++ server/utils/data.js
@@ -5,6 +5,7 @@ const calcData = (dataSets) => {
   for (let i = 0; i < totalDataSets; i++) {
 
     // Set dynamic server name
+    const isDeclining = Math.random() < 0.5;
     const serverId = `server_${i + 1}`;
     data[serverId] = [];
     for (let j = 0; j < 15; j++) {
@@ -12,7 +13,9 @@ const calcData = (dataSets) => {
         serverLoad: (j * 2 + 20),
         // Add i to make each dataset value a bit larger than previous
         // for presentation reasons
-        responseTime: Math.random() + j + (i * 3.5) + (Math.random() < 0.5 ? -1 : 1.2),
+        responseTime: isDeclining
+          ? Math.random() - j + (i * 4) + (Math.random() < 0.5 ? -1 : 1.2)
+          : Math.random() + j + (i * 4) + (Math.random() < 0.5 ? -1 : 1.2),
         processingPower: Math.random() + ((i + 1) * 3 + (Math.random() < 0.5 ? -0.25 : 0.5)),
       };
       data[serverId].push(set);
