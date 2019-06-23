6ee49608cdfb759046b70077f0089681a2dd0ae2
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-23 11:41:28 +0200

    refactor: format code

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import LinearRegressionChart from './components/LinearRegressionChart';
-import style from './App.module.css';
+import React, { useEffect, useState } from "react";
+import LinearRegressionChart from "./components/LinearRegressionChart";
+import style from "./App.module.css";
 
-const X_AXIS_LABEL = 'Server Load';
+const X_AXIS_LABEL = "Server Load";
 
 const App = () => {
-  const [columnType, setColumnType] = useState('responseTime');
-  const [data, setData] = useState(null);
-  const [showLines, setShowLines] = useState(null);
+    const [columnType, setColumnType] = useState("responseTime");
+    const [data, setData] = useState(null);
+    const [showLines, setShowLines] = useState(null);
 
-  useEffect(() => {
-    fetch('http://localhost:4000/api/v1/data')
-      .then(response => response.json())
-      .then(result => {
-        const { data } = result;
-        setData(data);
-      });
-  }, []);
+    useEffect(() => {
+        fetch("http://localhost:4000/api/v1/data")
+            .then(response => response.json())
+            .then(result => {
+                const { data } = result;
+                setData(data);
+            });
+    }, []);
 
-  const margin = {
-    top: 20,
-    right: 20,
-    bottom: 60,
-    left: 40
-  },
-  width = 600 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
-  const isResponseTime = columnType === 'responseTime';
+    const margin = {
+            top: 20,
+            right: 20,
+            bottom: 60,
+            left: 40
+        },
+        width = 600 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+    const isResponseTime = columnType === "responseTime";
 
-  return (
-    <div className={style.app}>
-      {data &&
-        <LinearRegressionChart
-          data={data}
-          columnType={columnType}
-          xAxisLabel={X_AXIS_LABEL}
-          margin={margin}
-          width={width}
-          height={height}
-          showLines={showLines}
-        />}
-        <button
-          className="button"
-          onClick={
-            () => {
-              setColumnType(isResponseTime ? 'processingPower' : 'responseTime');
-              setShowLines(false);
-            }
-          }
-        >
-          {isResponseTime ? 'Processing Power' : 'Response Time'}
-        </button>
-        <button
-          className="button"
-          onClick={
-            () => setShowLines(!showLines)
-          }
-        >
-          {showLines ? 'Hide Regressions' : 'Calculate Regressions'}
-      </button>
-    </div>
-  );
-}
+    return (
+        <div className={style.app}>
+            {data && (
+                <LinearRegressionChart
+                    data={data}
+                    columnType={columnType}
+                    xAxisLabel={X_AXIS_LABEL}
+                    margin={margin}
+                    width={width}
+                    height={height}
+                    showLines={showLines}
+                />
+            )}
+            <button
+                className="button"
+                onClick={() => {
+                    setColumnType(
+                        isResponseTime ? "processingPower" : "responseTime"
+                    );
+                    setShowLines(false);
+                }}
+            >
+                {isResponseTime ? "Processing Power" : "Response Time"}
+            </button>
+            <button className="button" onClick={() => setShowLines(!showLines)}>
+                {showLines ? "Hide Regressions" : "Calculate Regressions"}
+            </button>
+        </div>
+    );
+};
 
 export default App;
